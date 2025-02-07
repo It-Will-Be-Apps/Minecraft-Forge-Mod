3526fdb20d50cb165798445abe69fc4cc1b83557
WMartel <10606973+WMartel@users.noreply.github.com>
2025-02-07 18:48:01 +0000

    WiP

--- scripts/update.sh
+++ scripts/update.sh
@@ -15,17 +15,3 @@ echo 'Git updated!'
 echo 'Updating Java...'
 apt-get -y install default-jdk
 echo 'Java updated!'
-
-# Get the currently installed version of Java
-echo 'Removing older versions of Java...'
-latest_version=$(dpkg --list | grep openjdk | awk '{print $2}' | sort -V | tail -n 1 | grep -oP '\d+' | head -n 1)
-
-# Find and remove older versions of Java
-installed_jdks=$(dpkg --list | grep openjdk | awk '{print $2}')
-for jdk in $installed_jdks; 
-do
-    if [[ $jdk != *openjdk-"$latest_version"* ]]; then
-        apt-get -y purge $jdk
-    fi
-done
-echo 'Older versions of Java were removed...'
